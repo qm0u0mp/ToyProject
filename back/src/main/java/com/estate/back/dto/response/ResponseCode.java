41a62d8a12d5fe37fa137daafe48f413422f8f5e
qm0u0mp <99dngksmf@gmail.com>
2024-04-24 12:50:43 +0900

    2024.04.24

---
+++ back/src/main/java/com/estate/back/dto/response/ResponseCode.java
@@ -0,0 +1,6 @@
+package com.estate.back.dto.response;
+
+// Response의 공통된 code 값
+public interface ResponseCode {
+
+}
---
+++ back/src/main/java/com/estate/back/dto/response/ResponseDto.java
@@ -0,0 +1,6 @@
+package com.estate.back.dto.response;
+
+// Response의 공통 형태
+public interface ResponseDto {
+
+}
