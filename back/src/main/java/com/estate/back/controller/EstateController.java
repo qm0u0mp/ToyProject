0270c1f1fc834f1906b051949d7abc5e9832f861
qm0u0mp <99dngksmf@gmail.com>
2024-05-10 14:07:18 +0900

    2024.05.10

--- back/src/main/java/com/estate/back/controller/EstateController.java
+++ back/src/main/java/com/estate/back/controller/EstateController.java
@@ -3,9 +3,15 @@ package com.estate.back.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.estate.back.dto.response.estate.GetLocalDataResponseDto;
+import com.estate.back.dto.response.estate.GetRatioDataResponseDto;
 import com.estate.back.service.EstateService;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/estate")
@@ -13,5 +19,22 @@ import lombok.RequiredArgsConstructor;
 public class EstateController {
 
     private final EstateService estateService;
+
+    @GetMapping("/local/{local}")
+    public ResponseEntity<? super GetLocalDataResponseDto> getLocalData (
+        @PathVariable("local") String local
+    ) {
+        ResponseEntity<? super GetLocalDataResponseDto> response = estateService.getLocalData(local);
+        return response;
+    }
+
+    @GetMapping("/ratio/{local}")
+    public ResponseEntity<? super GetRatioDataResponseDto> getRatioDate(
+        @PathVariable("local") String local
+    ) {
+        ResponseEntity<? super GetRatioDataResponseDto> response = estateService.getRatioData(local);
+        return response;
+    }
+    
 
 }
--- back/src/main/java/com/estate/back/service/implementation/EstateServiceImplementation.java
+++ back/src/main/java/com/estate/back/service/implementation/EstateServiceImplementation.java
@@ -1,8 +1,16 @@
 package com.estate.back.service.implementation;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.estate.back.dto.response.ResponseDto;
+import com.estate.back.dto.response.estate.GetLocalDataResponseDto;
+import com.estate.back.dto.response.estate.GetRatioDataResponseDto;
 import com.estate.back.repository.EstateRepository;
+import com.estate.back.repository.resultSet.GetLocalDataResultSet;
+import com.estate.back.repository.resultSet.GetRatioDataResultSet;
 import com.estate.back.service.EstateService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,5 +20,32 @@ import lombok.RequiredArgsConstructor;
 public class EstateServiceImplementation implements EstateService{
     
     private final EstateRepository estateRepository;
+
+    @Override
+    public ResponseEntity<? super GetLocalDataResponseDto> getLocalData(String local) {
+
+        try{
+
+            List<GetLocalDataResultSet> resultSet = estateRepository.getLocalData(local);
+            return GetLocalDataResponseDto.success(resultSet);
+
+        } catch(Exception exception){
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+    }
+
+    @Override
+    public ResponseEntity<? super GetRatioDataResponseDto> getRatioData(String local) {
+        try{
+
+            List<GetRatioDataResultSet> resultSets = estateRepository.getRatioData(local);
+            return GetRatioDataResponseDto.success(resultSets);
+
+        } catch (Exception exception){
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+    }
     
 }
