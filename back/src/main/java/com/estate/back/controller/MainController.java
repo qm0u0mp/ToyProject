4eb73d2d0587d427ade04abdefbc1e6eeaf97dda
qm0u0mp <99dngksmf@gmail.com>
2024-06-04 12:43:24 +0900

    build

---
+++ back/src/main/java/com/estate/back/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.estate.back.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    
+    @Value("${deploy.env}")
+    private String DEPLOY_ENV;
+
+    @GetMapping("/")
+    public String serverCheck() {
+        return DEPLOY_ENV + "환경에서 실행 중 ...";
+    }
+    
+}
